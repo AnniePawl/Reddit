547c9c82be78c18008afecc6c7370ef9df633b38
Anna Pawl <annampawl@gmail.com>
2019-06-09 13:26:00 -0700

    users to save posts to db

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,11 +1,20 @@
 // POSTS CONTROLLER
 
 // Require Post Model
-// const Post = require('../models/post');
+const Post = require('../models/post');
 
-module.exports = app => {
+module.exports = (app) => {
+
   // CREATE
-  app.post("/posts/new", (req, res) => {
-    console.log(req.body);
+  app.post('/posts/new', (req, res) => {
+    // INSTANTIATE INSTANCE OF POST MODEL
+    const post = new Post(req.body);
+
+    // SAVE INSTANCE OF POST MODEL TO DB
+    post.save((err, post) => {
+      // REDIRECT TO THE ROOT
+      return res.redirect(`/`);
+    })
   });
+
 };
