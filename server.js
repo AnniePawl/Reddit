8a4efe7b10768cd7a7ffcb6a411899640e4b4988
Anna Pawl <annampawl@gmail.com>
2019-06-09 13:20:21 -0700

    installed body parser and express validator

--- server.js
+++ server.js
@@ -2,6 +2,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add after body parser initialization!
+app.use(expressValidator());
 
 // HANDLEBARS MATERIAL
 var exphbs = require('express-handlebars');
